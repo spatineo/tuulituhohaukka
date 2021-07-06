2adb0d12777628da1cda4b79cc2d8bd1caebce62
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-07-06 12:23:45 +0000

    Added types for loadData and setData actions

--- app/src/Store/Actions/data.ts
+++ app/src/Store/Actions/data.ts
@@ -1,11 +1,19 @@
 export const LOAD_DATA = 'LOAD_DATA'
 export const SET_DATA = 'SET_DATA'
 
-export const loadData = () => ({
+interface LoadDataAction {
+  type: string
+}
+interface setDataAction {
+  type:string
+  payload: JSON
+}
+
+export const loadData = ():LoadDataAction => ({
   type: LOAD_DATA
 })
 
-export const setData = (data: any) => ({
+export const setData = (data: JSON): setDataAction => ({
   type: SET_DATA, 
   payload: data,
 })
