836edbb88a937f85af753e5497b8ed201e8c1dde
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-07-06 10:11:01 +0000

    Created redux store and applied saga as middleware

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import './App.css';
 
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import createSagaMiddleware from '@redux-saga/core';
+import { Provider } from 'react-redux'
+
+import { rootSaga } from './Store/Sagas/rootSaga'
+import MainView from './Views/MainView'
+import dataReducer from './Store/Reducers/data';
+
+const rootReducer = combineReducers({
+  data: dataReducer
+})
+
+const configureStore = () => {
+  const sagaMiddleware = createSagaMiddleware()
+  const store = createStore(rootReducer, applyMiddleware(sagaMiddleware))
+  sagaMiddleware.run(rootSaga)
+  return store
+}
+
+const store = configureStore()
+
 function App() {
   return (
-    <div className="App">
-      <h1>Tuulituhohaukka 🌪 💥 🦅 </h1>
-    </div>
-  );
+    <Provider store={store}>
+      <MainView />
+    </Provider>
+  )
 }
 
 export default App;
