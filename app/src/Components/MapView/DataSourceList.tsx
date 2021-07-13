5a3c975cb9417929c8fbe11c6f4b905164924c86
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-07-13 07:54:29 +0000

    Created DataSourceList component skeleton

---
+++ app/src/Components/MapView/DataSourceList.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import { createStyles, makeStyles } from '@material-ui/styles'
+
+// interface interfaceName {
+//   value: string
+// }
+
+const DataSourceList: React.FC = () => {
+  const classes = useStyles()
+  return (
+    <div>
+      This is the data list open
+    </div>
+  )
+}
+
+const useStyles = makeStyles(() =>
+  createStyles({
+
+  }))
+
+export default DataSourceList
