53cd0c98230d2e7b79fa4f9fcdbb5ecaea2944fc
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-07-13 12:27:19 +0000

    Fetching data from redux and mapping listItems based on the data

--- app/src/Components/MapView/DataSourceList.tsx
+++ app/src/Components/MapView/DataSourceList.tsx
@@ -1,15 +1,31 @@
 import * as React from 'react'
 import { createStyles, makeStyles } from '@material-ui/styles'
+import { useSelector } from 'react-redux'
+
+import SourceDataListItem from './DataSourceListItem'
 
 // interface interfaceName {
 //   value: string
 // }
 
 const DataSourceList: React.FC = () => {
+  const [selectedValue, setSelectedValue] = React.useState('')
   const classes = useStyles()
+  const sources = useSelector((state: any) => state.data.cache.sources)
+  console.log('Source from redux are: ', sources)
+
+  const handleChange = (value: string) => {
+    setSelectedValue(value)
+  }
+
   return (
     <div>
-      This is the data list open
+      {sources.map((source: any) =>
+        <SourceDataListItem
+          selectedValue={selectedValue}
+          name={source.name}
+          onChange={handleChange}
+        />)}
     </div>
   )
 }
