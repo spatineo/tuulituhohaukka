b0d740384e1355392d3722fc9665552bef796611
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-07-13 13:32:03 +0000

    Added green color to radio button

--- app/src/Components/MapView/DataSourceListItem.tsx
+++ app/src/Components/MapView/DataSourceListItem.tsx
@@ -1,18 +1,30 @@
 import * as React from 'react'
-import { createStyles, makeStyles } from '@material-ui/styles'
-import { Radio } from '@material-ui/core'
+import { createStyles, makeStyles, withStyles } from '@material-ui/styles'
+import { green } from '@material-ui/core/colors';
+import { Radio, RadioProps } from '@material-ui/core'
 
 interface Props {
-  selectedValue: string
+  id: string
   name: string
   onChange: (value: string) => void
+  selectedValue: string
 }
 
-const DataSourceListItem: React.FC<Props> = ({ selectedValue, name, onChange }) => {
+const GreenRadio = withStyles({
+  root: {
+    color: green[400],
+    '&$checked': {
+      color: green[600],
+    },
+  },
+  checked: {},
+})((props: RadioProps) => <Radio color="default" {...props} />);
+
+const DataSourceListItem: React.FC<Props> = ({ name, onChange, selectedValue }) => {
   const classes = useStyles()
   return (
     <div className={classes.listItemContainer}>
-      <Radio
+      <GreenRadio
         checked={selectedValue === name}
         onChange={() => onChange(name)}
         value={name}
