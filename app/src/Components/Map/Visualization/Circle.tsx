5a44d89f2ce471b78ca5df5dc46ce4f8b983c49a
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-07-15 12:49:05 +0000

    Created Circle component

---
+++ app/src/Components/Map/Visualization/Circle.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react'
+import { createStyles, makeStyles } from '@material-ui/styles'
+
+interface Props {
+  text: string
+  color: string
+  borderWidth: string
+}
+
+const Circle: React.FC<Props> = ({ text, color, borderWidth }) => {
+  const classes = useStyles({ color, borderWidth })
+  return (
+    <div
+      className={classes.circle}
+      style={{ borderColor: `${color}`, borderWidth: `${borderWidth}` }}>
+      {text}
+    </div>
+  )
+}
+
+const useStyles = makeStyles<Props>(() =>
+  createStyles({
+    circle: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '80%',
+      aspectRatio: '1/1',
+      border: 'solid',
+      borderRadius: '50%',
+      background: 'rgb(247, 247, 247)',
+    },
+  }))
+
+export default Circle
