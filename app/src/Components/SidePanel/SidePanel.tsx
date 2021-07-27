9f1d40b30bfb695c138dfd7a73e07464e45baf84
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-07-27 13:14:30 +0000

    Rearranged elements

--- app/src/Components/SidePanel/SidePanel.tsx
+++ app/src/Components/SidePanel/SidePanel.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { createStyles, makeStyles } from '@material-ui/styles'
 import { Grid, Card } from '@material-ui/core'
 
-
+import SelectMonth from './SelectMonth'
 // interface SidePanelProps {
 
 // }
@@ -12,15 +12,12 @@ import SelectTime from './SelectTime'
 const SidePanel: React.FC = () => {
   const classes = useStyles()
   return (
-    <div>
-      <Grid
-        container
-        direction="column"
-        justify="center"
-        alignItems="center"
-        spacing={2}
-      >
-        <Grid item style={{ width: '90%' }}>
+    <div >
+      <Grid container direction="column" justify="center" alignItems="center" spacing={2} >
+        <Grid item >
+          <SelectMonth />
+        </Grid>
+        <Grid item style={{ width: '90%' }} >
           <Card style={{ paddingTop: '10px', paddingBottom: '10px' }}>
             <SelectTime />
           </Card>
