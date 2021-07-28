d1be598704471bb8c7ec151fa93908e5b38b9381
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-07-28 12:49:41 +0000

    Refactored Select month. Added list of buttons, which can be used to change the month

--- app/src/Components/SidePanel/SelectMonth.tsx
+++ app/src/Components/SidePanel/SelectMonth.tsx
@@ -1,19 +1,29 @@
 import * as React from 'react'
 import { createStyles, makeStyles } from '@material-ui/styles'
-import { Card, Grid } from '@material-ui/core'
+import { Typography, Card, Grid, CssBaseline, Button } from '@material-ui/core'
 import { Bar } from 'react-chartjs-2'
+import MonthElement from './MonthElement'
 
 // interface interfaceName {
 //   value: string
 // }
 
 const SelectMonth: React.FC = () => {
+
+  // 1. Get inspectionDate from Redux
+  // 2. Display month from inspection date as redMonth
+  // 3.1 onClick -> Edit inspectionDate based on the month clicked
+  // 3.2 Send action to set inspectionDate to Redux
+
   const [chartData, setChartData] = React.useState({})
+  const [selectedType, setSelectedType] = React.useState('inspection')
   const classes = useStyles()
+
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
   const chart = () => {
     setChartData({
-      labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+      labels: ['', '', '', '', '', '', '', '', '', '', '', ''],
       datasets: [
         {
           label: 'Wind Damages',
@@ -28,38 +38,70 @@ const SelectMonth: React.FC = () => {
     })
   }
 
+  const chartOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+    scales: {
+      x: {
+        ticks: {
+          display: false
+        },
+        grid: {
+          display: false
+        }
+      },
+      y: {
+        display: false,
+        grid: {
+          display: false,
+        }
+      }
+    }
+  }
+
   React.useEffect(() => {
     chart()
   }, [])
 
   return (
-    <Card style={{ width: '370px', padding: '10px' }}>
-      <Bar
-        data={chartData}
-        options={
-          {
-            responsive: true,
-            plugins: {
-              legend: {
-                display: false,
-              },
-            },
-            scales: {
-              x: {
-                grid: {
-                  display: false
-                }
-              },
-              y: {
-                display: false,
-                grid: {
-                  display: false,
-                }
-              }
-            }
-          }}
-      />
-    </Card>
+    <>
+      <CssBaseline />
+      <Card style={{ width: '400px', padding: '10px' }}>
+        <Bar
+          data={chartData}
+          options={chartOptions}
+        />
+        <Grid container spacing={1} justify='center'>
+          {months.map((month, index) => (
+            <Grid item xs={1} key={month} >
+              <MonthElement index={index} month={month} selectedType={selectedType} />
+            </Grid>
+          ))}
+          <Grid container item justify='space-around'>
+            <Grid item xs={6}>
+              <Button
+                color='default'
+                onClick={() => setSelectedType('comparison')}
+              >
+                <Typography>Set comparison</Typography>
+              </Button>
+            </Grid>
+            <Grid item xs={6}>
+              <Button
+                color='secondary'
+                onClick={() => setSelectedType('inspection')}
+              >
+                <Typography>Set inspection</Typography>
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Card>
+    </>
   )
 }
 
