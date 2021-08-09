a5dd9286fed3a940acea783f3475ceb23fd7b742
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-08-09 12:21:38 +0000

    Using axios to fetch json filed from public/TestData folder

--- app/src/Store/Sagas/loadDataSaga.ts
+++ app/src/Store/Sagas/loadDataSaga.ts
@@ -1,13 +1,19 @@
 import { takeLatest, call, put } from "@redux-saga/core/effects"
 import { LOAD_DATA } from "../Actions/data"
-import {setData} from '../Actions/data'
+import { setData } from '../Actions/data'
 
-import jsonFile from '../../TestData/redux-state.json'
-import dummyData from '../../TestData/dummy-data.json'
+// import jsonFile from '../../../public/TestData/redux-state.json'
+// import dummyData from '../../../public/TestData/dummy-data.json'
+import axios from "axios"
 
-const getData = async() => {
-  // return fetch('url').then(res => res.json())
-  return dummyData
+const getData = async () => {
+  const response = await axios.get('/TestData/dummy-data.json')
+  return response.data
+}
+
+const fetchRootCatalogue = async () => {
+  const response = await axios.get('/Testdata/root.json')
+  return response.data
 }
 
 export function* loadDataWatcher(): any {
