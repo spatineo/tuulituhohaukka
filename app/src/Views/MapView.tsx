3a6c450ee8d60be64a962971f56fb4ee6138a8d0
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-08-09 09:16:37 +0000

    new mapObject is created and passed into payload and to redux when add button is pressed

--- app/src/Views/MapView.tsx
+++ app/src/Views/MapView.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react'
+import { useDispatch } from 'react-redux'
 import { createStyles, makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 import { green } from '@material-ui/core/colors'
 import { Button, Grid } from '@material-ui/core'
 import { useSelector } from 'react-redux'
 import { Map } from '../types'
+
+import { addMap } from '../Store/Actions/data'
 
 import MapComponent from '../Components/Map/MapComponent'
 
@@ -20,8 +23,45 @@ const greenTheme = createMuiTheme({
 
 const MapView: React.FC = () => {
   const mapData = useSelector((state: any): Array<Map> => state.dataReducer.data.maps)
-
   const classes = useStyles()
+  const dispatch = useDispatch()
+
+  const latestMapIndex = mapData.length - 1
+
+  const payload = {
+    "mapObject": {
+      "id": latestMapIndex + 1,
+      "selectedSource": "id of the source",
+      "channelSettings": {
+        "R": "VV",
+        "G": "",
+        "B": "VH"
+      },
+      "displayWindDamageVector": true,
+      "displaySpyglass": false,
+      "panelBarSettings": {
+        "displayDataSourceList": false,
+        "displayVisualization": false
+      },
+
+      "derivedData": {
+        "sources": [],
+        "timeValues": {
+          "inspection": "",
+          "comparison": ""
+        },
+        "mapLayers": [
+          [
+            {
+              "linkToMapImage": "https://asdasdasd",
+              "srs": "EPSG:3067"
+            }
+          ]
+        ]
+      }
+    }
+  }
+
   return (
     <div className={classes.container}>
       <div className={classes.buttonContainer}>
@@ -30,6 +70,7 @@ const MapView: React.FC = () => {
             variant="contained"
             color="primary"
             style={{ aspectRatio: '1/1', color: 'white', fontWeight: 'bold', fontSize: '25px' }}
+            onClick={() => dispatch(addMap(payload))}
           >
             +
           </Button>
@@ -41,7 +82,7 @@ const MapView: React.FC = () => {
             return (
               <Grid key={mapObject.id} container direction='column' item xs={12} lg={6} xl={4} alignItems='center' >
                 <MapComponent
-                  data={mapObject}
+                  mapObject={mapObject}
                   mapComponentIndex={index}
                 />
               </Grid>)
