1f779ef7da72bd59462a1b58401ed59ac25223f2
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-08-10 12:18:01 +0000

    Added new actions for loading catalog

--- app/src/Store/Actions/data.ts
+++ app/src/Store/Actions/data.ts
@@ -1,7 +1,14 @@
 import dataReducer from "../Reducers/data"
 
+export const LOAD_ROOT_CATALOG = 'LOAD_ROOT_CATALOG'
+export const SET_ROOT_CATALOG = 'SET_ROOT_CATALOG'
+export const CATALOG_FETCH_START = 'CATALOG_FETCH_START'
+export const CATALOG_FETCH_FINISHED = 'CATALOG_FETCH_FINISHED'
+export const CATALOG_FETCH_FAILED = 'CATALOG_FETCH_FAILED'
+
 export const LOAD_DATA = 'LOAD_DATA'
 export const SET_DATA = 'SET_DATA'
+
 export const SET_RED_CHANNEL = 'SET_RED_CHANNEL'
 export const SET_GREEN_CHANNEL = 'SET_GREEN_CHANNEL'
 export const SET_BLUE_CHANNEL = 'SET_BLUE_CHANNEL'
@@ -25,6 +32,35 @@ interface SetColorChannelAction {
   payload: any
 }
 
+// _____ Catalog
+export const loadRootCatalog = (): LoadDataAction => ({
+  type: LOAD_ROOT_CATALOG
+})
+
+export const setRootCatalog = (data: JSON): SetDataAction => ({
+  type: SET_ROOT_CATALOG,
+  payload: data
+})
+
+export const catalogFetchStart = (data: any): SetDataAction => ({
+  type: CATALOG_FETCH_START,
+  payload: data
+})
+
+export const catalogFetchFinished = (data: any): SetDataAction => ({
+  type: CATALOG_FETCH_FINISHED,
+  payload: data
+})
+
+export const catalogFetchFailed = (data: any): SetDataAction => ({
+  type: CATALOG_FETCH_FAILED,
+  payload: data
+})
+
+
+
+
+//_____ Dummy Data
 export const loadData = (): LoadDataAction => ({
   type: LOAD_DATA
 })
@@ -34,6 +70,8 @@ export const setData = (data: JSON): SetDataAction => ({
   payload: data,
 })
 
+
+//_____ Functionality ______
 export const setRedChannel = (data: any): SetColorChannelAction => ({
   type: SET_RED_CHANNEL,
   payload: data
