8363c442d370068eb58bfd3cfbd8beabf1812bbf
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-08-11 13:40:55 +0000

    Created functions for fetching catalog

--- app/src/Store/Sagas/loadCatalogSaga.ts
+++ app/src/Store/Sagas/loadCatalogSaga.ts
@@ -1,33 +1,77 @@
 import axios from 'axios'
-import { useSelector } from 'react-redux'
-import { takeLatest, call, put } from "@redux-saga/core/effects"
+import { takeLatest, call, put, select } from "@redux-saga/core/effects"
 import { LOAD_ROOT_CATALOG } from '../Actions/data'
 import { setRootCatalog, catalogFetchStart, catalogFetchFailed, catalogFetchFinished } from '../Actions/data'
 
-// _____ Catalog Functions ______
+interface LoadDataActionWithParam {
+  type: string,
+  payload: string
+}
+
+// _____ Saga watchers and Workers ______
+
+export function* loadCatalogWatcher(): any {
+  yield takeLatest(LOAD_ROOT_CATALOG, loadCatalogWorker)
+}
+
+function* loadCatalogWorker(action: LoadDataActionWithParam): any {
+  const rootCatalog = yield call(getAllDatasets, action.payload)
+  yield put(setRootCatalog(rootCatalog))
+}
+
+// _____ Catalog functions
+const getAllDatasets = (url: string) => {
+  const rootCatalog = getCatalog(url)
+  const dataSets = produceDatasetResults(rootCatalog)
+  return dataSets
+}
+
+const getCatalog = async (url: string) => {
+  const catalog = select((state: any) => state.dataReducer.cache.catalog) as any
+  if (catalog[url]) return catalog[url]
+
+  if (!catalog[url])
+    put(catalogFetchStart({ url, inProgress: true }))
+  try {
+    const response = await axios.get(url)
+    const fetchedCatalog = response.data
+    put(catalogFetchFinished({ url, fetchedCatalog, inProgress: false }))
+  } catch (error) {
+    put(catalogFetchFailed({ url, error }))
+  }
+  return { links: [] }
+}
+
+
+export const produceDatasetResults = (rootCatalog: any) => {
+  return { datasets: rootCatalog.links.filter((link: any) => link.rel === 'child') }
+}
+
+
+
+
+
+
+//______ Return catalog
+
 export const fetchRootCatalog = async () => {
   const response = await axios.get('/Testdata/root.json')
   const rootCatalogue = response.data
   return rootCatalogue
 }
 
-export const produceDatasetResults = (rootCatalog: any) => {
-  return { datasets: rootCatalog.links.filter((link: any) => link.rel === 'child') }
-}
 
-export function* loadCatalogWatcher(): any {
-  yield takeLatest(LOAD_ROOT_CATALOG, loadCatalogWorker)
-}
 
-function* loadCatalogWorker(): any {
-  const rootCatalog = yield call(fetchRootCatalog)
-  yield put(setRootCatalog(rootCatalog))
-}
 
-function* getCatalog(url: string) {
+
+
+
+
+
+function* getCatalogGenerator(url: string) {
   // Fetch catalog from Redux
-  const catalog = useSelector((state: any): any => state.dataReducer.cache.catalog)
-  // If Catalog exists, return it
+  const getCatalogRedux = (state: any): any => state.dataReducer.cache.catalog
+  const catalog = select(getCatalogRedux) as any
   if (catalog[url]) return catalog
 
   // If No catalog, fetch catalog, and initially return empty array
@@ -41,19 +85,3 @@ function* getCatalog(url: string) {
     return { links: [] }
   }
 }
-
-// const getCatalog2 = async(url:string) => {
-//   const catalog = useSelector((state:any):any => state.dataReducer.cache.catalog)
-//   if (catalog[url]) return catalog[url]
-
-//   if (!catalog[url])
-//   put(catalogFetchStart({url, inProgress: true}))
-//   try {
-//     const response = await axios.get(url)
-//     const fetchedCatalog = response.data
-//     put(catalogFetchFinished({url, fetchedCatalog, inProgress: false}))
-//   } catch(error) {
-//     put(catalogFetchFailed({url, error}))
-//   }
-//   return {links: []}
-// }
