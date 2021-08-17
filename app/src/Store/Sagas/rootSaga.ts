4a3cdf8930682180d99414c38d6544e87aa96b0d
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-08-17 13:00:25 +0000

    Changed loadCatalog saga in rootSaga

--- app/src/Store/Sagas/rootSaga.ts
+++ app/src/Store/Sagas/rootSaga.ts
@@ -1,12 +1,14 @@
 import { fork, all } from 'redux-saga/effects'
 import { loadDummyDataWatcher } from './loadDummyDataSaga'
 import { loadCatalogWatcher } from './loadCatalogSaga'
+import { loadCatalogWatcher2 } from './loadCatalogSaga2'
+import { loadCatalogWatcherGenerator } from './loadCatalogSagaGenerator'
 
 export function* rootSaga() {
   yield all(
     [
       fork(loadDummyDataWatcher),
-      fork(loadCatalogWatcher)
+      fork(loadCatalogWatcherGenerator)
     ]
   )
 }
