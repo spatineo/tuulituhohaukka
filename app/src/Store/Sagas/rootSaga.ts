f419d2b15328b0ec3a006af7c92b4137d8c84f88
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-08-18 05:53:07 +0000

    Fixed root saga to work with renamed file

--- app/src/Store/Sagas/rootSaga.ts
+++ app/src/Store/Sagas/rootSaga.ts
@@ -1,14 +1,13 @@
 import { fork, all } from 'redux-saga/effects'
 import { loadDummyDataWatcher } from './loadDummyDataSaga'
+
 import { loadCatalogWatcher } from './loadCatalogSaga'
-import { loadCatalogWatcher2 } from './loadCatalogSaga2'
-import { loadCatalogWatcherGenerator } from './loadCatalogSagaGenerator'
 
 export function* rootSaga() {
   yield all(
     [
       fork(loadDummyDataWatcher),
-      fork(loadCatalogWatcherGenerator)
+      fork(loadCatalogWatcher)
     ]
   )
 }
