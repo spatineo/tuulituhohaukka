eaef78fafa41079cdc11e15824923cc795638efc
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-08-18 13:13:56 +0000

    Cleanup

--- app/src/Store/Sagas/loadDummyDataSaga.ts
+++ app/src/Store/Sagas/loadDummyDataSaga.ts
@@ -1,18 +1,13 @@
-import axios from "axios"
+import axios from 'axios'
 import { takeLatest, call, put } from "@redux-saga/core/effects"
 import { LOAD_DATA } from "../Actions/data"
 import { setData } from '../Actions/data'
 
-// import jsonFile from '../../../public/TestData/redux-state.json'
-// import dummyData from '../../../public/TestData/dummy-data.json'
-
-// _____ Dummy Data Functions _____
 const getDummyData = async () => {
   const response = await axios.get('/TestData/dummy-data.json')
   return response.data
 }
 
-// This will look for LOAD_DATA and run side effects once it is dispatched 
 export function* loadDummyDataWatcher(): any {
   console.log('loadDataWatcher called!')
   yield takeLatest(LOAD_DATA, loadDummyDataFlow)
@@ -23,9 +18,3 @@ function* loadDummyDataFlow(): any {
   const data = yield call(getDummyData)
   yield put(setData(data))
 }
-
-
-
-
-
-
