b20e30bc70423178896e95db3b7e1ae25b753eb7
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-08-18 06:29:58 +0000

    Refactored code

--- app/src/Store/Sagas/loadCatalogSaga.ts
+++ app/src/Store/Sagas/loadCatalogSaga.ts
@@ -1,14 +1,12 @@
 import { takeLatest, put, call, select } from '@redux-saga/core/effects'
 import axios from 'axios'
 import { LOAD_ROOT_CATALOG } from '../Actions/data'
-import { setRootCatalog, catalogFetchStart, catalogFetchFailed, catalogFetchFinished } from '../Actions/data'
+import { catalogFetchStart, catalogFetchFailed, catalogFetchFinished } from '../Actions/data'
 
 interface LoadDataActionWithParam {
   type: string,
   payload: string
 }
-
-// ______ Saga watcher _____
 
 // Once LOAD_ROOT_CATALOG action is detected, loadCatalogWorker is called
 export function* loadCatalogWatcher(): any {
@@ -19,13 +17,10 @@ export function* loadCatalogWatcher(): any {
 // Function will download the catalog if it is not found already from cache
 function* loadCatalogWorker(action: LoadDataActionWithParam): any {
   console.log('loadCatalogWorker called!')
-  const rootCatalog = yield call(getCatalog, action.payload)
-  // console.log('rootCatalog returned from getCatalog: ', rootCatalog)
-  // yield put(setRootCatalog(rootCatalog))
+  yield call(getCatalog, action.payload)
 }
 
-// Function will check if catalog is in cache, 
-// if not it will be downloaded and put into cahce
+// Function will check if catalog is in cache, if not it will be downloaded and put into cache
 function* getCatalog(url: string): any {
   console.log('getCatalog called')
   const catalog = yield select((state) => state.dataReducer.cache && state.dataReducer.cache.catalog)
@@ -37,8 +32,6 @@ function* getCatalog(url: string): any {
 
   // Check if fetch is in progress for given URL
   const fetchInProgress = yield select((state) => state.dataReducer.cache.fetchInProgress[url])
-  console.log('catalog: ', catalog[url])
-  console.log('fetchInProgress: ', fetchInProgress)
 
   if (!catalog[url] && !fetchInProgress) {
     // ---> This may create an issue with concurrent data fetching
@@ -54,16 +47,4 @@ function* getCatalog(url: string): any {
       yield put(catalogFetchFailed({ url, error }))
     }
   }
-  // return { links: [] }
-}
-
-
-
-
-
-function* getAllDatasets(url: string): any {
-  console.log('getAllDatasets called')
-  const rootCatalog = yield call(getCatalog, url)
-  const dataSets = rootCatalog.links.filter((link: any) => link.rel === 'child')
-  return dataSets
 }
