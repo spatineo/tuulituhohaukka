63ae2b9946468134f79fc50291aff97f089a44e6
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-08-24 13:30:30 +0000

    Removed console.logs

--- app/src/Store/Sagas/loadInitialSetupSaga.ts
+++ app/src/Store/Sagas/loadInitialSetupSaga.ts
@@ -4,19 +4,15 @@ import { LOAD_INITIAL_SETUP } from "../Actions/data"
 import { setInitialSetup } from '../Actions/data'
 
 const getInitialSetup = async () => {
-  console.log('Fetching initial setup')
   const response = await axios.get('/TestData/initialSetup.json')
   return response.data
 }
 
 export function* loadInitialSetupWatcher(): any {
-  console.log('loadDataWatcher called!')
   yield takeLatest(LOAD_INITIAL_SETUP, loadInitialSetupFlow)
 }
 
 function* loadInitialSetupFlow(): any {
-  console.log('LoadDataFlow called!')
   const data = yield call(getInitialSetup)
-  console.log('Dispatching initial setup..')
   yield put(setInitialSetup(data))
 }
