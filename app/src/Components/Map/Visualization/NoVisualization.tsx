2498cb7043f94dc0417f81eed54411cacd280356
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-08-25 07:31:33 +0000

    Cleanup

--- app/src/Components/Map/Visualization/NoVisualization.tsx
+++ app/src/Components/Map/Visualization/NoVisualization.tsx
@@ -1,15 +1,7 @@
 import * as React from "react";
-import { createStyles, makeStyles } from "@material-ui/styles";
-
-// interface interfaceName {
-//   value: string
-// }
 
 const NoVisualization: React.FC = () => {
-  const classes = useStyles();
   return <div>No Visualization</div>;
 };
-
-const useStyles = makeStyles(() => createStyles({}));
 
 export default NoVisualization;
