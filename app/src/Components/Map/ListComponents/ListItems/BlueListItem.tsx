c281f99bf53b64ce975cad8a6a3f4385a94052dc
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-08-25 12:51:28 +0000

    Added types, changes sources -> bands, cleanup

--- app/src/Components/Map/ListComponents/ListItems/BlueListItem.tsx
+++ app/src/Components/Map/ListComponents/ListItems/BlueListItem.tsx
@@ -1,19 +1,11 @@
 import * as React from 'react'
 import { useDispatch } from 'react-redux';
 import { setBlueChannel } from '../../../../Store/Actions/data'
-
 import { createStyles, makeStyles, withStyles } from '@material-ui/styles'
 import { blue } from '@material-ui/core/colors';
 import { Radio, RadioProps } from '@material-ui/core'
 import { ListChildComponentProps } from 'react-window'
 import { isNamedExports } from 'typescript';
-
-// interface Props {
-//   key: string
-//   name: string
-//   onChange: (value: string) => void
-//   selectedValue: string
-// }
 
 const BlueRadio = withStyles({
   root: {
@@ -26,27 +18,22 @@ const BlueRadio = withStyles({
 })((props: RadioProps) => <Radio color="default" {...props} />);
 
 const BlueListItem: React.FC<ListChildComponentProps> = ({ data, index, style }) => {
-  const title = data.sources[index].title
+  const name = data.bands[index].name
   const selectedValue = data.selectedValue
   const mapComponentIndex = data.mapComponentIndex
   const classes = useStyles()
   const dispatch = useDispatch()
 
-  const payload = {
-    mapComponentIndex: mapComponentIndex,
-    blueChannelValue: title
-  }
-
   return (
     <div className={classes.listItemContainer} style={style}>
       <BlueRadio
-        checked={selectedValue === title}
+        checked={selectedValue === name}
         onChange={() => {
-          dispatch(setBlueChannel(payload))
+          dispatch(setBlueChannel({ mapComponentIndex: mapComponentIndex, blueChannelValue: name }))
         }}
         value={isNamedExports}
       />
-      {title}
+      {name}
     </div>
   )
 }
