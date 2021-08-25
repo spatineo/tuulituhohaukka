8efb934855044ce01a1abea91668e2471ccfe627
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-08-25 07:34:59 +0000

    Removed unnecassary css, selectedSource -> selectedDataset + cleanup

--- app/src/Views/MapView.tsx
+++ app/src/Views/MapView.tsx
@@ -6,11 +6,7 @@ import { green } from '@material-ui/core/colors'
 import { Button, Grid } from '@material-ui/core'
 import { useSelector } from 'react-redux'
 import { Map } from '../types'
-import MapComponent from '../Components/Map/MapComponent'
-
-// interface interfaceName {
-//   value: string
-// }
+import MapComponent from '../Components/Map/General/MapComponent'
 
 const greenTheme = createMuiTheme({
   palette: {
@@ -22,13 +18,12 @@ const MapView: React.FC = () => {
   const classes = useStyles()
   const dispatch = useDispatch()
   const mapData = useSelector((state: any): Array<Map> => state.dataReducer.data.maps)
-
   const latestMapIndex = mapData.length - 1
 
   const payload = {
     "mapObject": {
       "id": latestMapIndex + 1,
-      "selectedSource": "id of the source",
+      "selectedDataset": "id of the source",
       "channelSettings": {
         "R": "VV",
         "G": "",
@@ -66,26 +61,24 @@ const MapView: React.FC = () => {
           <Button
             variant="contained"
             color="primary"
-            style={{ aspectRatio: '1/1', color: 'white', fontWeight: 'bold', fontSize: '25px' }}
+            style={{ color: 'white', fontWeight: 'bold', fontSize: '25px' }}
             onClick={() => dispatch(addMap(payload))}
           >
             +
           </Button>
         </ThemeProvider>
       </div>
-      <div className={classes.mapsContainer}>
-        <Grid container justify='flex-start' spacing={6} style={{ width: '100%' }}>
-          {mapData.map((mapObject, index) => {
-            return (
-              <Grid key={mapObject.id} container direction='column' item xs={12} lg={6} xl={4} alignItems='center' >
-                <MapComponent
-                  mapObject={mapObject}
-                  mapComponentIndex={index}
-                />
-              </Grid>)
-          })}
-        </Grid>
-      </div>
+      <Grid container justify='flex-start' spacing={6} style={{ width: '100%', height: '100%' }}>
+        {mapData.map((mapObject, index) => {
+          return (
+            <Grid key={mapObject.id} container direction='column' item xs={12} lg={6} xl={4} alignItems='center' >
+              <MapComponent
+                mapObject={mapObject}
+                mapComponentIndex={index}
+              />
+            </Grid>)
+        })}
+      </Grid>
     </div>
   )
 }
@@ -94,23 +87,14 @@ const useStyles = makeStyles(() =>
   createStyles({
     container: {
       display: 'flex',
-      flexDirection: 'row',
     },
     buttonContainer: {
-      height: '100%',
-      aspectRatio: '1/1',
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'center',
-      justifyContent: 'flex-start',
       marginLeft: '10px',
       marginRight: '10px',
     },
-    mapsContainer: {
-      height: '100%',
-      width: '100%',
-      // border: 'solid blue 1px'
-    }
   }))
 
 export default MapView
