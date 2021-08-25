a710aa2df8167d9f654115be222358c9c35abdde
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-08-25 07:30:29 +0000

    Renamed SourceListItem -> DatasetListItem

--- app/src/Components/Map/ListComponents/ListItems/DatasetListItem.tsx
+++ app/src/Components/Map/ListComponents/ListItems/DatasetListItem.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react'
 import { useDispatch, batch } from 'react-redux';
-
 import { createStyles, makeStyles, withStyles } from '@material-ui/styles'
 import { green } from '@material-ui/core/colors';
 import { Radio, RadioProps } from '@material-ui/core'
 import { ListChildComponentProps } from 'react-window'
-
-import { setDataSource, setBands } from '../../../../Store/Actions/data'
+import { setSelectedDataset, setBands } from '../../../../Store/Actions/data'
 import { getBandsForDataset } from '../../../../API/Api';
 
 const GreenRadio = withStyles({
@@ -19,26 +17,22 @@ const GreenRadio = withStyles({
   checked: {},
 })((props: RadioProps) => <Radio color="default" {...props} />);
 
-const SourceListItem: React.FC<ListChildComponentProps> = ({ data, index, style }) => {
+const DatasetListItem: React.FC<ListChildComponentProps> = ({ data, index, style }) => {
   const title = data.sources[index].title
-  const selectedSource = data.selectedSource
+  const selectedDataset = data.selectedDataset
   const sourceId = data.sources[index].id
   const mapComponentIndex = data.mapComponentIndex
   const classes = useStyles()
   const dispatch = useDispatch()
 
-
-
-  console.log('Selected source in sourceList is:', selectedSource)
-
   return (
     <div className={classes.listItemContainer} style={style}>
       <GreenRadio
-        checked={selectedSource === sourceId}
+        checked={selectedDataset === sourceId}
         onChange={() => {
           console.log('Sending action from component number: ', index)
           batch(() => {
-            dispatch(setDataSource({ mapComponentIndex: mapComponentIndex, selectedSource: sourceId }))
+            dispatch(setSelectedDataset({ mapComponentIndex: mapComponentIndex, selectedDataset: sourceId }))
             dispatch(setBands({ bands: getBandsForDataset(sourceId), mapComponentIndex: mapComponentIndex }))
           }
           )
@@ -60,4 +54,4 @@ const useStyles = makeStyles(() =>
     }
   }))
 
-export default SourceListItem
+export default DatasetListItem
