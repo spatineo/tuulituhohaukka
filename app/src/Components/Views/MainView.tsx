7300ec7d8427fb79fbcd1ab808b9d79b8595ec53
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-08-26 14:14:02 +0000

    WIP  created createUrl functionality

--- app/src/Components/Views/MainView.tsx
+++ app/src/Components/Views/MainView.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { loadInitialSetup, setAllDatasets, setBands } from '../../Store/Actions/data'
 import { Button, Divider, Grid } from '@material-ui/core'
 import { makeStyles, createStyles } from '@material-ui/core/styles';
@@ -7,10 +7,29 @@ import { Typography } from '@material-ui/core';
 import MapView from './MapView'
 import SidePanel from './SidePanel'
 import { getAllDatasets, getBandsForDataset } from '../../API/Api';
+import { RootState } from '../../App';
 
 const MainView: React.FC = () => {
   const classes = useStyles()
   const dispatch = useDispatch()
+  const globalState = useSelector((state: RootState) => state.dataReducer.data.global)
+  const mapArray = useSelector((state: RootState) => state.dataReducer.data.maps)
+
+  console.log('MapArray froms state: ', mapArray)
+  // 1. Get wanted state from redux
+  // 2. Run state through JSON.stringify
+  // 3. Compress the string somehow..
+
+  const createUrl = (globalState: any, mapArray: any) => {
+    const updatedArray = mapArray.map((mapObject: any) => {
+      console.log('mapObject in loop: ', mapObject)
+      Object.keys(mapObject)
+        .filter((key: string) => key === 'derivedData')
+    })
+    console.log('Updated array: ', updatedArray)
+  }
+
+  createUrl(globalState, mapArray)
 
   React.useEffect(() => {
     dispatch(loadInitialSetup())
