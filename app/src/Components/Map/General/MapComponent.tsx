93d0d8e3406a24b0dbf72ca045d0917ff6ab28df
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-08-26 07:19:59 +0000

    MapComponent gets datasets from props, removed unnecassary div

--- app/src/Components/Map/General/MapComponent.tsx
+++ app/src/Components/Map/General/MapComponent.tsx
@@ -10,66 +10,61 @@ import { Map } from '../../../types'
 import { removeMap } from '../../../Store/Actions/data'
 import DatasetList from '../ListComponents/Lists/DatasetList'
 import NormalVisualization from '../Visualization/NormalVisualization'
-import { getAllDatasets } from '../../../API/Api'
 
 interface Props {
   mapObject: Map,
   mapComponentIndex: number
+  datasets: Dataset[]
 }
 
-const MapComponent: React.FC<Props> = ({ mapObject, mapComponentIndex }) => {
-  const datasets = getAllDatasets() as Dataset[]
-  const cache = useSelector((state: RootState) => state.dataReducer.cache)
+const MapComponent: React.FC<Props> = ({ mapObject, mapComponentIndex, datasets }) => {
+
   const dateFromRedux = useSelector((state: RootState): string => state.dataReducer.data.global.inspectionDate)
   const editedDate = new Date(dateFromRedux).toISOString().split("T")[0]
   const classes = useStyles()
   const dispatch = useDispatch()
 
-  React.useEffect(() => {
-    getAllDatasets()
-  }, [cache])
+
 
   return (
-    <div>
-      <div className={classes.mapContainer}>
-        <div className={classes.mapBox}>
-          <Button
-            style={{ position: 'absolute', zIndex: 2, maxWidth: '35px', minWidth: '35px', maxHeight: '35px', minHeight: '35px', right: '0px' }} variant="contained" color="secondary"
-            onClick={() => dispatch(removeMap({ id: mapObject.id }))}
-          >
-            -
-          </Button>
-          <OpenLayersMap />
-        </div>
-        <div className={classes.footer}>
-          <Grid container>
-            <Grid container item xs={4} justify='center' alignItems='center' spacing={1}>
-              <Grid item >
-                <div style={{ color: '#00a9f7', fontSize: '11px' }}>another date </div>
-              </Grid>
-              <Grid item>
-                <div style={{ color: '#ff0000', fontSize: '11px' }}>{editedDate}</div>
-              </Grid>
+    <div className={classes.mapContainer}>
+      <div className={classes.mapBox}>
+        <Button
+          style={{ position: 'absolute', zIndex: 2, maxWidth: '35px', minWidth: '35px', maxHeight: '35px', minHeight: '35px', right: '0px' }} variant="contained" color="secondary"
+          onClick={() => dispatch(removeMap({ id: mapObject.id }))}
+        >
+          -
+        </Button>
+        <OpenLayersMap />
+      </div>
+      <div className={classes.footer}>
+        <Grid container>
+          <Grid container item xs={4} justify='center' alignItems='center' spacing={1}>
+            <Grid item >
+              <div style={{ color: '#00a9f7', fontSize: '11px' }}>another date </div>
             </Grid>
-            <Grid container item xs={4} justify='center' alignItems='center'>
-              <div style={{ fontSize: '14px' }}>Tuulituhotunnistus</div>
-            </Grid>
-            <Grid container item xs={4} justify='center'>
-              <div style={{ fontSize: '14px', border: 'solid black 1px', borderRadius: ' 5px', padding: '6px' }}>button</div>
+            <Grid item>
+              <div style={{ color: '#ff0000', fontSize: '11px' }}>{editedDate}</div>
             </Grid>
           </Grid>
+          <Grid container item xs={4} justify='center' alignItems='center'>
+            <div style={{ fontSize: '14px' }}>Tuulituhotunnistus</div>
+          </Grid>
+          <Grid container item xs={4} justify='center'>
+            <div style={{ fontSize: '14px', border: 'solid black 1px', borderRadius: ' 5px', padding: '6px' }}>button</div>
+          </Grid>
+        </Grid>
+      </div>
+      <div className={classes.menuContainer}>
+        <div className={classes.dropDown}>
+          <SlimAccordion name={'Aineistot'}>
+            <DatasetList datasets={datasets} mapComponentIndex={mapComponentIndex} />
+          </SlimAccordion>
         </div>
-        <div className={classes.menuContainer}>
-          <div className={classes.dropDown}>
-            <SlimAccordion name={'Aineistot'}>
-              <DatasetList datasets={datasets} mapComponentIndex={mapComponentIndex} />
-            </SlimAccordion>
-          </div>
-          <div className={classes.dropDown}>
-            <SlimAccordion name={'Visualisointi'}>
-              <NormalVisualization channelSettings={mapObject.channelSettings} mapComponentIndex={mapComponentIndex} />
-            </SlimAccordion>
-          </div>
+        <div className={classes.dropDown}>
+          <SlimAccordion name={'Visualisointi'}>
+            <NormalVisualization channelSettings={mapObject.channelSettings} mapComponentIndex={mapComponentIndex} />
+          </SlimAccordion>
         </div>
       </div>
     </div>
