7b17cb5c45957813f4dd4413e1040d63c58a804b
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-08-30 13:36:14 +0000

    WIP - Added AppBar and Drawer to Main View

--- app/src/Components/Views/MainView.tsx
+++ app/src/Components/Views/MainView.tsx
@@ -1,18 +1,42 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { loadInitialSetup, setStateFromUrl } from '../../Store/Actions/data'
-import { Button, Divider, Grid } from '@material-ui/core'
-import { makeStyles, createStyles } from '@material-ui/core/styles';
+import { AppBar, Button, Divider, Drawer, Grid, IconButton, Toolbar, List, ListItem } from '@material-ui/core'
+import clsx from 'clsx';
+import MenuIcon from '@material-ui/icons/Menu'
+import { makeStyles, createStyles, useTheme } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
 import MapView from './MapView'
 import SidePanel from './SidePanel'
 import { RootState } from '../../App';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import GraphIcon from '@material-ui/icons/Assessment';
+import CalendarIcon from '@material-ui/icons/Today';
+
+import SelectDay from '../SidePanel/SelectDay';
+import SelectMonth from '../SidePanel/SelectMonth';
+
+const drawerWidth = 370
 
 const MainView: React.FC = (props: any) => {
   const classes = useStyles()
   const dispatch = useDispatch()
   const globalState = useSelector((state: RootState) => state.dataReducer.data.global)
   const mapArray = useSelector((state: RootState) => state.dataReducer.data.maps)
+
+  const theme = useTheme()
+  const [open, setOpen] = React.useState(false)
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
   const createUrl = () => {
     const filteredMaps = mapArray.map((mapObject, index) => {
@@ -64,45 +88,202 @@ const MainView: React.FC = (props: any) => {
 
   return (
     <div className="App">
-      <Typography variant='h4'>Tuulituhohaukka 🌪 💥 🦅 </Typography>
-      <h1></h1>
-      <Button variant='contained' onClick={() => {
-        window.prompt('Copy the link from here ⬇️', createUrl())
-      }}>
-        Copy URL to clipboard
-      </Button>
-      <Divider />
-      <div className={classes.root}>
-        <Grid container className={classes.container}>
-          <Grid item xs={4} lg={3} xl={2} className={classes.border}>
-            <SidePanel />
-          </Grid>
-          <Grid item xs={8} lg={9} xl={10} >
-            <MapView />
-          </Grid>
-        </Grid>
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, {
+              [classes.hide]: open,
+            })}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Tuulituhohaukka 🌪 💥 🦅
+          </Typography>
+          <Button
+            variant='contained'
+            onClick={() => {
+              window.prompt('Copy the link from here ⬇️', createUrl())
+            }}>
+            Copy URL to clipboard
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open,
+        })}
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          }),
+        }}
+      >
+        <div className={classes.toolbar}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          <ListItem  >
+            <Button size='medium' onClick={open ? handleDrawerClose : handleDrawerOpen}>
+              <ListItemIcon>
+                <GraphIcon />
+              </ListItemIcon>
+            </Button>
+            <div className={clsx(classes.graphContent, {
+              [classes.graphContentShift]: open
+            })}>
+              <SelectMonth />
+            </div>
+          </ListItem>
+
+          <ListItem >
+            <Button onClick={open ? handleDrawerClose : handleDrawerOpen}>
+              <ListItemIcon>
+                <CalendarIcon />
+              </ListItemIcon>
+            </Button>
+            <div className={clsx(classes.graphContent, {
+              [classes.graphContentShift]: open
+            })}>
+              <SelectDay />
+            </div>
+          </ListItem>
+          <Divider />
+        </List>
+      </Drawer>
+      <div className={clsx(classes.content, {
+        [classes.contentShift]: open,
+      })}>
+        <div className={classes.toolbar} />
+        <MapView />
       </div>
     </div>
   )
 }
 
-const useStyles = makeStyles(() =>
+const useStyles = makeStyles((theme) =>
   createStyles({
-    reduxLoadButton: {
-      margin: '50px',
-    },
+    // reduxLoadButton: {
+    //   margin: '50px',
+    // },
+    // root: {
+    //   display: 'flex',
+    //   flexGrow: 1,
+    //   height: '1000px',
+    // },
+    // border: {
+    //   border: 'solid black 1px',
+    //   borderRadius: '10px'
+    // },
+    // container: {
+    //   height: '100%'
+    // },
     root: {
+      display: 'flex',
+    },
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+    appBarShift: {
+      marginLeft: drawerWidth,
+      width: `calc(100% - ${drawerWidth}px)`,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    menuButton: {
+      marginRight: 36,
+    },
+    hide: {
+      display: 'none',
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+      whiteSpace: 'nowrap',
+    },
+    drawerOpen: {
+      width: drawerWidth,
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    drawerClose: {
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      overflowX: 'hidden',
+      width: theme.spacing(7) + 1,
+      [theme.breakpoints.up('sm')]: {
+        width: theme.spacing(9) + 1,
+      },
+    },
+    toolbar: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'flex-end',
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+    },
+    content: {
       flexGrow: 1,
-      height: '1000px',
+      padding: theme.spacing(3),
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      marginLeft: 50,
     },
-    border: {
-      border: 'solid black 1px',
-      borderRadius: '10px'
+    contentShift: {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: drawerWidth,
     },
-    container: {
-      height: '100%'
+
+
+    graphContent: {
+
+      padding: theme.spacing(2),
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      margin: 0,
+    },
+    graphContentShift: {
+
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: -35,
     }
-  })
+  }),
 )
 
 export default MainView;
