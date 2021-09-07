0200ecf86d14b93f4838e829792695b5a6a20c49
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-09-07 07:08:03 +0000

    Created logic that will clear band selections if another dataset is selected

--- app/src/Components/Map/Visualization/NormalVisualization.tsx
+++ app/src/Components/Map/Visualization/NormalVisualization.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react'
-import { useSelector } from 'react-redux'
+import { batch, useDispatch, useSelector } from 'react-redux'
 import { RootState } from '../../../App'
 import { Grid } from '@material-ui/core'
 import ChannelColorTile from './ChannelColorTile'
 import SlimAccordion from '../General/SlimAccordion'
 import BandList from '../ListComponents/Lists/BandList'
 import { ChannelSettings } from '../../../types'
+import { setRedChannel, setGreenChannel, setBlueChannel } from '../../../Store/Actions/data'
 
 interface Props {
   channelSettings: ChannelSettings
@@ -13,19 +14,44 @@ interface Props {
 }
 
 const NormalVisualization: React.FC<Props> = ({ mapComponentIndex }) => {
+  const dispatch = useDispatch()
   const colorData = useSelector((state: RootState) => state.dataReducer.data.maps[mapComponentIndex].channelSettings)
   const [clickedColorTile, setClickedColorTile] = React.useState('')
   const bands = useSelector((state: RootState) => state.dataReducer.data.maps[mapComponentIndex].derivedData.bands)
   const selectedDataset = useSelector((state: RootState) => state.dataReducer.data.maps[mapComponentIndex].selectedDataset)
 
-  // ToDO
-  // 1. When selectedDataset changes check if
-  // 2. Check if bands are the same
-  // 3. If so, do nothing
-  // 4. If different, fire actions that will set R, G, B to empty string ""
-  // React.useEffect(() => {
+  // Will compare arrays and elements whether they are equal
+  const arrayEquals = (array1: any, array2: any) => {
+    return Array.isArray(array1)
+      && Array.isArray(array2)
+      && array1.length === array2.length
+      && array1.every((object, index) => object.name === array2[index].name)
+  }
 
-  // }, [selectedDataset])
+  // Stores and gives access to previous state
+  const usePrevious = (value: any) => {
+    const ref = React.useRef()
+    React.useEffect(() => {
+      ref.current = value
+    })
+    return ref.current
+  }
+
+  const previousBands = usePrevious(bands)
+  console.log('bands: ', bands, 'vs ', 'previous bands: ', previousBands)
+
+  // If elements are not equal inside arrays, reset selections in color bubbles
+  React.useEffect(() => {
+    if (previousBands === undefined) return
+
+    if (!arrayEquals(previousBands, bands)) {
+      batch(() => {
+        dispatch(setRedChannel({ mapComponentIndex: mapComponentIndex, redChannelValue: '' }))
+        dispatch(setGreenChannel({ mapComponentIndex: mapComponentIndex, greenChannelValue: '' }))
+        dispatch(setBlueChannel({ mapComponentIndex: mapComponentIndex, blueChannelValue: '' }))
+      })
+    }
+  }, [selectedDataset])
 
   const setClicked = (value: string) => {
     setClickedColorTile(value)
