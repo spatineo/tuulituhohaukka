47d9fb534c80587288ed29be574d96bb05070ace
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-09-20 12:39:15 +0000

    Refactored ChannelColorTile

--- app/src/Components/Map/Visualization/ChannelColorTile.tsx
+++ app/src/Components/Map/Visualization/ChannelColorTile.tsx
@@ -13,23 +13,16 @@ interface Props {
 const ChannelColorTile: React.FC<Props> = ({ text, letter, color, setClicked }) => {
   const classes = useStyles()
   return (
-    <div>
-      <Grid container direction='column' justify='center' alignItems='center'>
-        <Grid container item direction='row' xs={8} style={{ width: '100%', aspectRatio: '10/12' }}>
-          <Grid container item xs={4} direction='column' justify='center' style={{ paddingTop: '10px' }} >
-            {letter + ':'}
-          </Grid>
-          <Grid item xs={8} direction='column' justify='center' alignItems='center'>
-            <div
-              className={classes.ballContainer}
-              onClick={() => setClicked(letter)}
-            >
-              <Circle text={text} color={color} borderWidth={'1'} />
-            </div>
-          </Grid>
-        </Grid>
+    <Grid container>
+      <Grid item xs={12}>
+        <div
+          className={classes.ballContainer}
+          onClick={() => setClicked(letter)}
+        >
+          <Circle text={text} color={color} borderWidth={'1'} />
+        </div>
       </Grid>
-    </div >
+    </Grid>
   )
 }
 
@@ -38,8 +31,6 @@ const useStyles = makeStyles(() =>
     ballContainer: {
       display: 'flex',
       height: '100%',
-      padding: '5px',
-      aspectRatio: '1/1',
       flexDirection: 'column',
       justifyContent: 'center',
       alignItem: 'center',
