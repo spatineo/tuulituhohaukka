93e8317d91812173ad541ac9382a9859ac9687b2
Sampo Savolainen <sampo.savolainen@spatineo.com>
2021-09-24 10:50:34 +0000

    Switch to FMI catalog

--- app/src/API/Api.ts
+++ app/src/API/Api.ts
@@ -1,7 +1,7 @@
 import { Dataset } from '../types'
 
-const CATALOG_ROOT = 'https://s3.eu-west-1.amazonaws.com/directory.spatineo.com/tmp/tuulituhohaukka-stac/catalog/root2.json'
-//const CATALOG_ROOT = 'https://pta.data.lit.fmi.fi/stac/root.json'
+//const CATALOG_ROOT = 'https://s3.eu-west-1.amazonaws.com/directory.spatineo.com/tmp/tuulituhohaukka-stac/catalog/root2.json'
+const CATALOG_ROOT = 'https://pta.data.lit.fmi.fi/stac/root.json'
 
 interface Link {
   rel: string
